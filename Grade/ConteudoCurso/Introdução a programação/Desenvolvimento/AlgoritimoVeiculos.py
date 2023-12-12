574d65c980ee841bbb9e02f805893d1c87354668
PaulaSena <senasilvaanapaula@gmail.com>
2023-12-12 20:44:58 -0300

    CategoriaHabilitacao

---
+++ Grade/ConteudoCurso/Introdução a programação/Desenvolvimento/AlgoritimoVeiculos.py
@@ -0,0 +1,14 @@
+
+
+qtdRodas=4
+peso=2500
+qtdPessoas=2
+
+# categoria A
+if peso <3500
+print()
+if qtdRodas == 4:
+if qtdPessoas == 1: 
+CategoriaA=
+
+https://docs.google.com/document/d/1khpUgluS3nrc_FfZDnRPSj2IQpEcKBLga_qQmjeTvvM/edit
